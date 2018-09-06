f48b1d48439273bf6545986fedf61a36ed9adb11
Agnes Jang <agnes.jang@sendgrid.com>
2018-09-06 09:59:16 -0700

    Made comment bar be able to move up with keyboard

--- lib/pages/prtimelineview.dart
+++ lib/pages/prtimelineview.dart
@@ -4,73 +4,106 @@ import '../github/graphql.dart';
 import '../github/pullrequest.dart';
 import '../github/timeline.dart';
 
-class PRTimelineView extends StatelessWidget {
+class PRTimelineView extends StatefulWidget {
   final Future<List<TimelineItem>> prTimelineList;
   final PullRequest pr;
 
   PRTimelineView(this.prTimelineList, this.pr);
 
   @override
+    State<StatefulWidget> createState() {      
+      return PRTimelineViewState();
+    }
+
+}
+
+class PRTimelineViewState extends State<PRTimelineView> {
+  String comment;
+  TextEditingController _textEditingController = new TextEditingController();
+  @override
   Widget build(BuildContext context) {
-    String comment;
     return Scaffold(
-        appBar: AppBar(title: Text('${pr.title}')),
+        appBar: AppBar(title: Text('${widget.pr.title}')),
         body:
-            FutureBuilder(
-            future: prTimelineList, builder: _buildPRTimelineList),      
-        // floatingActionButton: FloatingActionButton(
-        //   backgroundColor: Theme.of(context).primaryColorDark,
-        //   child: Icon(Icons.add_comment),
-        //   foregroundColor: Colors.white,
-        //   elevation: 5.0,
-        //   onPressed: () {
-        //       showBottomSheet(
-        //         context: context,
-        //         builder: (BuildContext context) {
-        //           return Column(
-        //             children: <Widget>[
-        //               TextField(
-        //                 decoration: InputDecoration(labelText: "Enter comment here"),
-        //                 keyboardType: TextInputType.multiline,
-        //               )
-        //             ],
-        //           );
-        //         });
-        //   },
-        // ),
-        bottomNavigationBar: BottomAppBar(
-          child: new Row(
-            children: <Widget>[
-              new Expanded (
-                child: Container(
-                padding: EdgeInsets.only(left: 10.0),
-                child:
-                TextField(
-                  decoration: InputDecoration(labelText: "Enter comment here"),
-                  keyboardType: TextInputType.multiline,
-                  onChanged: (String c) {
-                    comment = c;
-                  },
-                  onSubmitted: (String c) {
-                    comment = c;
-                  }
-                ),
-                width: MediaQuery.of(context).size.width*5/8,
-              ),
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width/10,
-              ),
-              RaisedButton(
-                child: Text("Comment"),
-                color: Theme.of(context).primaryColorLight,
-                onPressed: () {
-                  addComment(null, pr, comment);
-                },
-              )
-            ],
+          Column (
+            children: <Widget> [
+              Flexible(child: FutureBuilder(
+                future: widget.prTimelineList,
+                builder: _buildPRTimelineList
+              )),
+              Divider(height: 1.0),
+              Container(
+                child: new Row(
+                  children: <Widget>[
+                    new Expanded (
+                      child: Container(
+                      padding: EdgeInsets.only(left: 10.0),
+                      child:
+                      TextField(
+                        controller: _textEditingController,
+                        decoration: InputDecoration(labelText: "Enter comment here"),
+                        keyboardType: TextInputType.multiline,
+                        onChanged: (String c) {
+                          comment = c;
+                        },
+                        onSubmitted: (String c) {
+                          comment = c;
+                          addComment(null, widget.pr, comment);
+                          _textEditingController.clear();
+                        }
+                      ),
+                      width: MediaQuery.of(context).size.width*5/8,
+                    ),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width/10,
+                    ),
+                    RaisedButton(
+                      child: Text("Comment"),
+                      color: Theme.of(context).primaryColorLight,
+                      onPressed: () {
+                        addComment(null, widget.pr, comment);
+                        _textEditingController.clear();
+                      },
+                    )
+                  ], 
+              ))
+            ]
           )
-        ),
+            
+        // bottomNavigationBar: BottomAppBar(
+        //   child: new Row(
+        //     children: <Widget>[
+        //       new Expanded (
+        //         child: Container(
+        //         padding: EdgeInsets.only(left: 10.0),
+        //         child:
+        //         TextField(
+        //           decoration: InputDecoration(labelText: "Enter comment here"),
+        //           keyboardType: TextInputType.multiline,
+        //           onChanged: (String c) {
+        //             comment = c;
+        //           },
+        //           onSubmitted: (String c) {
+        //             comment = c;
+        //           }
+        //         ),
+        //         width: MediaQuery.of(context).size.width*5/8,
+        //       ),
+        //       ),
+        //       SizedBox(
+        //         width: MediaQuery.of(context).size.width/10,
+        //       ),
+        //       RaisedButton(
+        //         child: Text("Comment"),
+        //         color: Theme.of(context).primaryColorLight,
+        //         onPressed: () {
+        //           addComment(null, pr, comment);
+        //         },
+        //       )
+        //     ],
+        //   )
+        // ),
         );
   }
 
@@ -78,7 +111,7 @@ class PRTimelineView extends StatelessWidget {
       BuildContext context, AsyncSnapshot<List<TimelineItem>> snapshot) {
     if (snapshot.connectionState == ConnectionState.done) {
       return snapshot.data.length != 0
-          ? PRTimelineList(snapshot.data, this.pr)
+          ? PRTimelineList(snapshot.data, widget.pr)
           : Center(child: Text('No timeline for this PR!'));
     } else {
       return Center(child: CircularProgressIndicator());
