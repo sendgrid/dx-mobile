4e5a00ab55459bf0d84356d749f90a7e4f0a0010
Agnes Jang <agnes.jang@sendgrid.com>
2018-09-12 16:45:36 -0700

    added pull down refresh to PR listview

--- lib/pages/prlistview.dart
+++ lib/pages/prlistview.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 import '../github/pullrequest.dart';
 import '../github/timeline.dart';
@@ -14,66 +15,93 @@ class PRListView extends StatefulWidget {
   PRListView(this.owner, this.repoName, this.prList);
   @override
   State<StatefulWidget> createState() {
-    return PRListViewState();
+    return PRListViewState(prList);
   }
 }
 
 class PRListViewState extends State<PRListView> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text('Pull Request List')),
-        body: FutureBuilder(future: widget.prList, builder: _buildPRList));
+
+  Future<List<PullRequest>> prList;
+  RefreshController rc = new RefreshController();
+
+  PRListViewState(this.prList);
+
+  Widget _createPRListWidget(BuildContext context, List<PullRequest> prs) {
+    return SmartRefresher(
+      enablePullDown: true,
+      onRefresh: _refreshPRList,
+      controller: rc,
+      child: ListView(
+        children: prs
+            .map((pullRequest) => Container(
+                    child: ListTile(
+                  title: Text("${pullRequest.title} #${pullRequest.number}"),
+                  subtitle: Text("${pullRequest.author}"),
+                  onTap: () {
+                    // call query that displays page with the PR's info
+                    Future<List<TimelineItem>> timelines =
+                        getPRTimeline(pullRequest);
+                    // display them
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              PRTimelineView(timelines, pullRequest),
+                        ));
+                  },
+                  //trailing: StarWidget(pullRequest.repo.starCount),
+                )))
+            .toList(),
+      )
+    );
   }
 
-// TO-DO later on: pull-down refresh should update PR list and also send it back to dashboard. or
-// do it in dashboard and update it again, might need to do some research to see which is better
-  _refreshPRList() async {
-    // have to add any new PRs one by one
-    // call getPRs
-    List<PullRequest> newPRs = await getPRs(widget.owner, widget.repoName);
+
+  void _refreshPRList(bool b) {
+      prList = getPRs(widget.owner, widget.repoName);
+      //rc.sendBack(true, RefreshStatus.completed); // makes it break, but works without.
+      // can look into making this better later on
+
+    Navigator.pushReplacement(context, 
+      PageRouteBuilder(
+        pageBuilder: (BuildContext context, Animation<double> animation,
+          Animation<double> secondAnimation) {
+            return PRListView(widget.owner, widget.repoName, prList);
+          },
+        transitionsBuilder: (BuildContext context, Animation<double> animation, 
+        Animation<double> secondAnimation, Widget child) {
+          return FadeTransition(
+            opacity: Tween(begin: 0.0, end: 10.0).animate(animation),
+            child: child
+          );
+        }
+
+        ),
+      );
+    b = true;
   }
 
   Widget _buildPRList(
       BuildContext context, AsyncSnapshot<List<PullRequest>> snapshot) {
     if (snapshot.connectionState == ConnectionState.done) {
       return snapshot.data.length != 0
-          ? PRList(snapshot.data)
-          : Center(child: Text('No PRs for you!'));
+          ? _createPRListWidget(context, snapshot.data)
+          : SmartRefresher(
+              enablePullDown: true,
+              onRefresh: _refreshPRList,
+              controller: rc,
+              child: ListView(children: <Widget>[Text('No PRs for you!')],)
+            );
     } else {
       return Center(child: CircularProgressIndicator());
     }
   }
-}
-
-class PRList extends StatelessWidget {
-  final List<PullRequest> prs;
-
-  PRList(this.prs);
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: prs
-          .map((pullRequest) => Container(
-                  child: ListTile(
-                title: Text("${pullRequest.title} #${pullRequest.number}"),
-                subtitle: Text("${pullRequest.author}"),
-                onTap: () {
-                  // call query that displays page with the PR's info
-                  Future<List<TimelineItem>> timelines =
-                      getPRTimeline(pullRequest);
-                  // display them
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) =>
-                            PRTimelineView(timelines, pullRequest),
-                      ));
-                },
-                //trailing: StarWidget(pullRequest.repo.starCount),
-              )))
-          .toList(),
-    );
+    return Scaffold(
+        appBar: AppBar(title: Text('Pull Request List')),
+        body: FutureBuilder(future: prList, builder: _buildPRList));
   }
+
 }
