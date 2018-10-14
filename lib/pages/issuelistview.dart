bd9e9770e3426a130f4dc5d2ffd82d18b4584e23
MaskyS <kufahmeeran@gmail.com>
2018-10-14 21:52:01 +0400

    Tidy up and remove extra Container widget.

--- lib/pages/issuelistview.dart
+++ lib/pages/issuelistview.dart
@@ -15,66 +15,37 @@ class IssueListView extends StatefulWidget {
   IssueListView(this.owner, this.repoName, this.issueList);
 
   @override
-  State<StatefulWidget> createState() {
-    return IssueListViewState(issueList);
-  }
+  State<StatefulWidget> createState() => IssueListViewState(issueList);
 }
 
 class IssueListViewState extends State<IssueListView> {
   Future<List<Issue>> issueList;
-  RefreshController rc = new RefreshController();
+  RefreshController rc = RefreshController();
 
   IssueListViewState(this.issueList);
 
-  Widget _createIssueListWidget(BuildContext context, List<Issue> issues) {
-    return SmartRefresher(
-        enablePullDown: true,
-        onRefresh: _refreshIssueList,
-        controller: rc,
-        child: ListView(
-          children: issues
-              .map((issue) => Container(
-                      child: ListTile(
-                    title: Text("${issue.title} #${issue.number}"),
-                    subtitle: Text(issue.author),
-                    onTap: () {
-                      Future<List<TimelineItem>> timelines =
-                          getIssueTimeline(issue);
-                      // display them
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) =>
-                                IssueTimelineView(timelines, issue),
-                          ));
-                    },
-                  )))
-              .toList(),
-        ));
-  }
-
-  void _refreshIssueList(bool b) {
-    issueList = getIssues(widget.owner, widget.repoName);
-    //rc.sendBack(true, RefreshStatus.completed); // makes it break, but works without.
-    // can look into making this better later on
-
-    Navigator.pushReplacement(
-      context,
-      PageRouteBuilder(pageBuilder: (BuildContext context,
-          Animation<double> animation, Animation<double> secondAnimation) {
-        return IssueListView(widget.owner, widget.repoName, issueList);
-      }, transitionsBuilder: (BuildContext context, Animation<double> animation,
-          Animation<double> secondAnimation, Widget child) {
-        return FadeTransition(
-            opacity: Tween(begin: 0.0, end: 10.0).animate(animation),
-            child: child);
-      }),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: FutureBuilder(future: issueList, builder: _buildIssueList),
     );
-    b = true;
   }
+
+  AppBar _buildAppBar() => AppBar(
+        title: Text('Issue List'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      );
 
   Widget _buildIssueList(
-      BuildContext context, AsyncSnapshot<List<Issue>> snapshot) {
+    BuildContext context,
+    AsyncSnapshot<List<Issue>> snapshot,
+  ) {
     if (snapshot.connectionState == ConnectionState.done) {
       return snapshot.data.length != 0
           ? _createIssueListWidget(context, snapshot.data)
@@ -84,23 +55,73 @@ class IssueListViewState extends State<IssueListView> {
               controller: rc,
               child: ListView(
                 children: <Widget>[Text('No issues for you!')],
-              ));
+              ),
+            );
     } else {
       return Center(child: CircularProgressIndicator());
     }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-            title: Text('Issue List'),
-            leading: new IconButton(
-              icon: new Icon(Icons.arrow_back_ios, color: Colors.white),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            )),
-        body: FutureBuilder(future: issueList, builder: _buildIssueList));
+  void _refreshIssueList(bool b) {
+    issueList = getIssues(widget.owner, widget.repoName);
+    //rc.sendBack(true, RefreshStatus.completed); // makes it break, but works without.
+    // can look into making this better later on
+
+    Navigator.pushReplacement(
+      context,
+      PageRouteBuilder(pageBuilder: (
+        BuildContext context,
+        Animation<double> animation,
+        Animation<double> secondAnimation,
+      ) {
+        return IssueListView(
+          widget.owner,
+          widget.repoName,
+          issueList,
+        );
+      }, transitionsBuilder: (
+        BuildContext context,
+        Animation<double> animation,
+        Animation<double> secondAnimation,
+        Widget child,
+      ) {
+        return FadeTransition(
+          opacity: Tween(begin: 0.0, end: 10.0).animate(animation),
+          child: child,
+        );
+      }),
+    );
+
+    b = true;
+  }
+
+  Widget _createIssueListWidget(BuildContext context, List<Issue> issues) {
+    return SmartRefresher(
+      enablePullDown: true,
+      onRefresh: _refreshIssueList,
+      controller: rc,
+      child: ListView(
+        children: issues
+            .map(
+              (issue) => ListTile(
+                    title: Text("${issue.title} #${issue.number}"),
+                    subtitle: Text(issue.author),
+                    onTap: () {
+                      Future<List<TimelineItem>> timelines =
+                          getIssueTimeline(issue);
+                      // display them
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              IssueTimelineView(timelines, issue),
+                        ),
+                      );
+                    },
+                  ),
+            )
+            .toList(),
+      ),
+    );
   }
 }
